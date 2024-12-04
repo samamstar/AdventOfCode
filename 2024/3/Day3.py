c7cd078ad475dcbb9ed7bcae63aa90c0374bec2f
samamstar <samamstar@gmail.com>
2024-12-03 21:50:43 -0700

    Day 3

---
+++ 2024/3/Day3.py
@@ -0,0 +1,22 @@
+import re
+
+with open("/home/sam/Git/AdventOfCode/2024/3/prompt.txt") as f:
+    stream = f.read()
+
+total = 0
+for command in re.findall(r"mul\(\d+,\d+\)", stream):
+    numbers = re.findall(r"\d+", command)
+    total += int(numbers[0]) * int(numbers[1])
+print("Part1:", total)
+
+total = 0
+mulOn = True
+for command in re.findall(r"(mul\(\d+,\d+\)|do\(\)|don't\(\))", stream):
+    if(command == "don't()"):
+        mulOn = False
+    elif(command == "do()"):
+        mulOn = True
+    elif(mulOn):
+        numbers = re.findall(r"\d+", command)
+        total += int(numbers[0]) * int(numbers[1])
+print("Part2:", total)
