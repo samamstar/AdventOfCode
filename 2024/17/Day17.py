f106e33b6269956cefc304147ec8c50215171c1e
samamstar <samamstar@gmail.com>
2024-12-17 13:23:18 -0700

    Day17 gave up

--- 2024/17/Day17.py
+++ 2024/17/Day17.py
@@ -75,21 +75,39 @@ Restart if A > 0
 """
 Copy A mod 8 to B
 XOR B with 011
-OUT A/2^B
+OUT A/2^B mod 8
 Divide A by 8
 Restart if A > 0
 """
 
-# So, work backwards, Sam.
+# So what does this tell me?
+# Unless B is zero on that out, then out will always be even?
+# Because 2^B will always be... wait shit no that's just wrong
+# Some of these operations are destructive, I don't think I can just run this backwards to get the original A
+# Makes sense, there's multiple possible As for each output
+
+# Hmm what if there are patterns in the binary...
+# A of 55593699 is          011 010 100 000 100 101 011 100 011 (Added a leading zero to match length)
+# Outputs 6,0,6,3,0,2,3,1,6 110 000 110 011 000 010 011 001 110
+# OK so that means our final A result will need to be 16*3 = 48 bits (Well, 47 or 46 depending on leading zeroes)
+# I don't think that works, because there's other patterns that don't hold
+
+# A of 55593699 is          011 010 100 000 100 101 011 100 011 (Added a leading zero to match length)
+# Output triplets reversed: 110 001 011 010 000 011 110 000 110 
+# Still no dice. The 011s on the A match the 110s on the output, but 100 results in 000 or 011
+
+# outString = "" 
+# for outVal in reversed(output):
+#     outString += "{:03b} ".format(outVal)
+# print(outString)
+
+# I think I need to solve this problem differently
 
 finalA = 0
 for outVal in reversed(program):
     finalA *= 8
 
-    # Find the smallest B value that will output outVal (Hopefully always less than 8?)
-    bVal = 0
-    while(finalA/ 2**(bVal ^ 3) != outVal):
-        bVal += 1
+    while(finalA):
+        pass
     
-    finalA += bVal
 print("Part2:", finalA)
